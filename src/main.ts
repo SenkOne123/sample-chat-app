c1feebaf795feb31a4c76b5a7cbcceee57a41c4e
Nikita Mikryukov <senkone0123@gmail.com>
2023-03-19 22:20:19 +0500

    feat: Try to fix deploy bugs

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors();
+
     const config = new DocumentBuilder()
         .setTitle('Sample Chat App')
         .addTag('Sample chat 1.0')
