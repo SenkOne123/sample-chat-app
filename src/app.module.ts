0fb67dad48930a4e1cddd6c0acb769d9a1902a9d
Nikita Mikryukov <senkone0123@gmail.com>
2023-03-19 20:42:35 +0500

    feat: Try to fix deploy bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,6 @@ import { UserRoles } from './modules/authentication/roles/user-roles.model';
         SequelizeModule.forRoot({
             dialect: 'postgres',
             host: process.env.POSTGRES_HOST,
-            port: +process.env.POSTGRES_PORT,
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
